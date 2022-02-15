ee71dfb0f2c6fbee5e9a3f770933e3106da8ba83
mbellini33 <mbellini33@gmail.com>
2022-02-15 09:43:45 +0100

    Update streamlit_app_test.py

--- PolyInstaloaderStreamlit/streamlit_app_test.py
+++ PolyInstaloaderStreamlit/streamlit_app_test.py
@@ -1,10 +1,22 @@
-import streamlit as st
+from dash import Dash, html, dcc
+import plotly.express as px
+
 import pandas as pd
 import numpy as np
 
 
-data_frame = pd.read_csv("../uber.csv")
-data_frame.rename(columns={'Lat':'lat','Lon':'lon'},inplace=True)
-map_data = data_frame[["lat", "lon"]]
 
-st.map(map_data)
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+#fig.show()
+
+app = Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)
