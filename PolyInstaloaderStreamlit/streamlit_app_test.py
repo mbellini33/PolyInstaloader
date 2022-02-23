c1da2ec85c37de70611aa45e763fd7a3983531b9
mbellini33 <mbellini33@gmail.com>
2022-02-23 23:45:00 +0100

    fix import data

--- PolyInstaloaderStreamlit/streamlit_app_test.py
+++ PolyInstaloaderStreamlit/streamlit_app_test.py
@@ -3,23 +3,39 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import requests
 
 
+#Query dataset dal progetto con l'indirizzo corretto
+
+r = requests.get("https://api.mapbox.com/datasets/v1/federicogodino/ckzikmhdh1ivt22tetuk76y20/features?access_token=sk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6eW02azFvMDFvMDNjcXBkdWppdW9tbiJ9.u715QLdqAeEZzDCpfl2AYg")
+#
+
+####ELABORAZIONE DA JSON A DATAFRAME
+
+data = [[
+              45.522788,
+            9.093885
+
+            ]]
+
+us_cities = pd.DataFrame(data,columns=['lat','lon'])
 
 
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-
-
-
-fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon",
                         color_discrete_sequence=["red"], zoom=3, height=300)
 
+#fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+#                        color_discrete_sequence=["red"], zoom=3, height=300)
 
-scatter = fig.data[0]
+
+
+
+#scatter = fig.data[0]
 fig.update_mapboxes(style="mapbox://styles/federicogodino/ckziqrfug006i14nyovs0ee76",accesstoken="pk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6Y3hyazgwMjNvejJvbnh0ZXptbTJkbSJ9.HBsPU_tmnC_VbsAK4FA1XQ")
 fig.update_layout(margin={"r":.5,"t":0.5,"l":0.5,"b":0.5})
 
-scatter.on_click()
+#scatter.on_click()
 
 
 """
