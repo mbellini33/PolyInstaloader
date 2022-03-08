65c56ae820d45b1ef8f7e94a995fbc2d26d162db
mbellini33 <mbellini33@gmail.com>
2022-03-08 12:09:01 +0100

    update 8/3

---
+++ PolyInstaloaderStreamlit/back_up.py
@@ -0,0 +1,16 @@
+"""
+html.Div([
+        dcc.Markdown("""
+** Click
+Data **
+
+Click
+on
+points in the
+graph.
+"""),
+html.Pre(id='click-data',
+         className='three columns')
+]),
+"""
+
--- PolyInstaloaderStreamlit/streamlit_app_test.py
+++ PolyInstaloaderStreamlit/streamlit_app_test.py
@@ -4,65 +4,107 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 import requests
-
-
+import json
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash_table as dt
+import dash_bootstrap_components as dbc
 #Query dataset dal progetto con l'indirizzo corretto
 
-r = requests.get("https://api.mapbox.com/datasets/v1/federicogodino/ckzikmhdh1ivt22tetuk76y20/features?access_token=sk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6eW02azFvMDFvMDNjcXBkdWppdW9tbiJ9.u715QLdqAeEZzDCpfl2AYg")
-#
-
-####ELABORAZIONE DA JSON A DATAFRAME
-
-data = [[
-              45.522788,
-            9.093885
-
-            ]]
-
-us_cities = pd.DataFrame(data,columns=['lat','lon'])
+#Sistemiamo il dataset
+r = requests.get("https://api.mapbox.com/datasets/v1/federicogodino/cl0f4sd5j08r022pnj5t7fx3d/features?access_token=sk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6eW02azFvMDFvMDNjcXBkdWppdW9tbiJ9.u715QLdqAeEZzDCpfl2AYg")
 
 
-fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon",
-                        color_discrete_sequence=["red"], zoom=3, height=300)
+data = r.text
 
-#fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
-#                        color_discrete_sequence=["red"], zoom=3, height=300)
+data = json.loads(data)
+data = data['features']
+input_data = pd.DataFrame(columns=['lat','lon','name','media'])
+
+for i in data:
+    row = dict.fromkeys(['lat','lon'])
+    row['lat'] = i['geometry']['coordinates'][1]
+    row['lon'] = i['geometry']['coordinates'][0]
+    input_data = input_data.append(row,ignore_index=True)
+
+input_data['name'] = 'Punto 1'
+input_data['media'] = 'LINKPROVA'
 
 
+fig = px.scatter_mapbox(input_data, lat="lat", lon="lon",
+                        hover_name="name",
+                        hover_data={"media":True,
+                                    "lat":False,
+                                    "lon":False
+                                    },
+                        color_discrete_sequence=["green"],
+                        zoom=11,
+                        height=300)
 
-
-#scatter = fig.data[0]
 fig.update_mapboxes(style="mapbox://styles/federicogodino/ckziqrfug006i14nyovs0ee76",accesstoken="pk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6Y3hyazgwMjNvejJvbnh0ZXptbTJkbSJ9.HBsPU_tmnC_VbsAK4FA1XQ")
-fig.update_layout(margin={"r":.5,"t":0.5,"l":0.5,"b":0.5})
+fig.update_layout(clickmode='event+select')
 
-#scatter.on_click()
-
-
-"""
-def update_point(trace, points, selector):
-    c = list(scatter.marker.color)
-    s = list(scatter.marker.size)
-    for i in points.point_inds:
-        c[i] = '#bae2be'
-        s[i] = 20
-        with f.batch_update():
-            scatter.marker.color = c
-            scatter.marker.size = s
-
-
-"""
+#fig.update_layout(margin={"r":.5,"t":0.5,"l":0.5,"b":0.5})
 
 
 
-fig.show()
-
-
-
-"""
 app = Dash()
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    dcc.Graph(
+              id='basic-interactions',
+              figure=fig),
+
+    html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+            """),
+            html.Pre(id='click-data'),
+        ], className='three columns'),
+
+html.Div([
+            dcc.Markdown("""
+                **Selection Data**
+
+                Choose the lasso or rectangle tool in the graph's menu
+                bar and then select points in the graph.
+
+                Note that if `layout.clickmode = 'event+select'`, selection data also
+                accumulates (or un-accumulates) selected data if you hold down the shift
+                button while clicking.
+            """),
+            html.Pre(id='selected-data'),
+        ], className='three columns'),
+
 ])
 
+#TODO : mettergli il risultato di instaloader
+
+@app.callback(
+    Output("click-data", "children"),
+    Input('basic-interactions', 'clickData'))
+def display_click_data(clickData):
+    # INSTAGRAPI
+    from instagrapi import Client
+    import os
+    user = 'instafinderbot'
+    passwd = 'instaloader'
+
+    cl = Client()
+    cl.login(user, passwd)
+
+    location = '570664753'
+
+    #Filtrare per category sul comune id
+
+
+
+
+
+
+    return json.dumps(clickData,indent=2)
+
 app.run_server(debug=True, use_reloader=False)
-"""
+
