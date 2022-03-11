d2b1d8aeb304d86e032b6bab22bb13b197899408
mbellini33 <mbellini33@gmail.com>
2022-03-11 15:25:41 +0100

    update

--- app.py
+++ app.py
@@ -1,26 +1,88 @@
 from dash import Dash, dcc, html, Input, Output
-import os
+import requests
+import json
+import pandas as pd
+import plotly.express as px
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+r = requests.get("https://api.mapbox.com/datasets/v1/federicogodino/cl0f4sd5j08r022pnj5t7fx3d/features?access_token=sk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6eW02azFvMDFvMDNjcXBkdWppdW9tbiJ9.u715QLdqAeEZzDCpfl2AYg")
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+data = r.text
 
+data = json.loads(data)
+data = data['features']
+input_data = pd.DataFrame(columns=['lat','lon','name','media'])
+
+for i in data:
+    row = dict.fromkeys(['lat','lon'])
+    row['lat'] = i['geometry']['coordinates'][1]
+    row['lon'] = i['geometry']['coordinates'][0]
+    input_data = input_data.append(row,ignore_index=True)
+
+input_data['name'] = 'Punto 1'
+input_data['media'] = 'LINKPROVA'
+
+
+fig = px.scatter_mapbox(input_data, lat="lat", lon="lon",
+                        hover_name="name",
+                        hover_data={"media":True,
+                                    "lat":False,
+                                    "lon":False
+                                    },
+                        color_discrete_sequence=["green"],
+                        zoom=11,
+                        height=300)
+
+fig.update_mapboxes(style="mapbox://styles/federicogodino/ckziqrfug006i14nyovs0ee76",accesstoken="pk.eyJ1IjoiZmVkZXJpY29nb2Rpbm8iLCJhIjoiY2t6Y3hyazgwMjNvejJvbnh0ZXptbTJkbSJ9.HBsPU_tmnC_VbsAK4FA1XQ")
+fig.update_layout(clickmode='event+select')
+
+#fig.update_layout(margin={"r":.5,"t":0.5,"l":0.5,"b":0.5})
+
+
+
+app = Dash(__name__)
 server = app.server
 
+
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
-    html.Div(id='display-value')
+    dcc.Graph(
+              id='basic-interactions',
+              figure=fig),
+
+    html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+            """),
+            html.Pre(id='click-data'),
+        ], className='three columns'),
+
+html.Div([
+            dcc.Markdown("""
+                **Selection Data**
+
+                Choose the lasso or rectangle tool in the graph's menu
+                bar and then select points in the graph.
+
+                Note that if `layout.clickmode = 'event+select'`, selection data also
+                accumulates (or un-accumulates) selected data if you hold down the shift
+                button while clicking.
+            """),
+            html.Pre(id='selected-data'),
+        ], className='three columns'),
+
 ])
 
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+#TODO : mettergli il risultato di instaloader
+
+@app.callback(
+    Output("click-data", "children"),
+    Input('basic-interactions', 'clickData'))
+
+def display_click_data(clickData):
+    return 'PORCO DIO OVINO'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
